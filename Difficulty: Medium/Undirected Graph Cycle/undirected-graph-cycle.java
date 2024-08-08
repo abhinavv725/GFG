ef12b4871dd2907cdf272750bbb8879a5d8328fb
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2024-08-08 21:53:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -31,44 +31,47 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Pair {
-    int node,parent;
-    Pair(int node, int parent){
+class Pair{
+    int parent,node;
+    Pair(int node,int parent){
         this.node=node;
         this.parent=parent;
     }
 }
 
 class Solution {
-    
-    private boolean bfs(int src,boolean[] vis, ArrayList<ArrayList<Integer>> adj){
+    private boolean bfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis){
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(src,-1));
-        vis[src]=true;
+        q.add(new Pair(node,-1));
         while(!q.isEmpty()){
-            int node = q.peek().node;
-            int parent = q.peek().parent;
-            q.remove();
-            for(Integer it: adj.get(node)){
-                if(!vis[it]){
-                    q.add(new Pair(it,node));
-                    vis[it]=true;
-                }
+            Pair p = q.poll();
+            int num=p.node;
+            int parent=p.parent;
+            vis[num]=true;
+            for(Integer it: adj.get(num)){
+                if(!vis[it])
+                    q.add(new Pair(it,num));
                 else if(it!=parent)
                     return true;
             }
+            
         }
         return false;
     }
-    
+    // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean[] vis = new boolean[V];
+        
+        boolean[] vis= new boolean[V];
+        
+        
         for(int i=0;i<V;i++){
-            if(vis[i]==false){
-                if(bfs(i,vis,adj)==true)
+            if(!vis[i]){
+                if(bfs(i,adj,vis)){
                     return true;
+                }
             }
         }
         return false;
+    
     }
 }
