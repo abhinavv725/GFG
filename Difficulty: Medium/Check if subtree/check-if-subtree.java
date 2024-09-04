53366e3149cd63c9cb67d386b7bcdaddcbba5e17
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2024-09-04 22:06:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check if subtree/check-if-subtree.java
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+                
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+	        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+	        while(t-- > 0){
+	            String tt= br.readLine();
+	            Node rootT = buildTree(tt);
+	            
+	            String s= br.readLine();
+	            Node rootS = buildTree(s);
+	           // printInorder(root);
+	            
+	            Solution tr=new Solution();
+	            boolean st=tr.isSubtree(rootT, rootS);
+	            if(st==true){
+	                System.out.println("1");
+	            }else{
+	                System.out.println("0");
+	            }
+	        }
+	}
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    private static boolean equal(Node t, Node s){
+        if(t==null && s==null)return true;
+        if(t==null || s==null)return false;
+        if(t.data!=s.data)
+            return false;
+        
+        
+        return equal(t.left,s.left) && equal(t.right, s.right);
+    }
+    public static boolean isSubtree(Node t, Node s) {
+        if (t == null) {
+            return false; // If main tree is null, s can't be a subtree
+        }
+        if (s == null) {
+            return true; // Null tree is always a subtree
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(t);
+        while(!q.isEmpty()){
+            Node x= q.poll();
+            if(x.data==s.data){
+                if(equal(x,s)==true)return true;
+            }
+            if(x.left!=null)q.add(x.left);
+            if(x.right!=null)q.add(x.right);
+        }
+        return false;
+    }
+}
